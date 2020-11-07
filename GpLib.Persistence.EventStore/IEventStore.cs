b998264cbf9f386afcf9a3a0327a7f15aef97c5a
ahmad.korbesa <ahmad.korbesa@neowave.com.my>
2020-11-07 21:27:40 +0100

    Added simple event store

---
+++ GpLib.Persistence.EventStore/IEventStore.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GpLib.Persistence.EventStore
+{
+    public interface IEventStore
+    {
+        Task<IEventStream> CreateStream(string streamId);
+        
+        Task<IEventStream> LoadEventStream(string streamId, int version);
+
+        Task AppendToStream(string streamId, int exptectedVersion, IEnumerable<IEvent> events);
+    }
+}
